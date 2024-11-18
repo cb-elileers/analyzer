56f2a0669a30159eb7fca52eaa5fa64390a1590b
Eli Leers <elias.leers@coinbase.com>
2024-11-18 15:59:04 -0800

    Commander SARIF support

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,13 @@ import main from './main';
 import { program } from 'commander';
 
 program
-  .argument('[string]', 'Path were the contracts lies')
-  .option('-s, --scope <string>', '.txt file containing the contest scope')
-  .option('-g, --github <string>', 'github url to generate links to code')
-  .option('-o, --out <string>', 'where to save the report')
+  .argument('[basePath]', 'Path were the contracts lies')
+  .option('-s, --scope <scopeFile>', '.txt file containing the contest scope')
+  .option('-g, --github <githubURL>', 'github url to generate links to code')
+  .option('-o, --out <reportPath>', 'Path for Markdown report')
+  .option('-l, --listfiles', 'List analyzed files in Markdown Report')
+  .option('--legacyscope <scopeFile>', 'Path for legacy scope file')
+  .option('--sarif [string]', 'Generate SARIF report, optionally include path to report. Default is analyzer.sarif', )
   .action((basePath:string, options) => {
     basePath = basePath ||'contracts/';
     basePath = basePath.endsWith('/') ? basePath : `${basePath}/`;
@@ -23,7 +26,15 @@ program
     console.log(`scope: ${options.scope||'----'}`);
     console.log(`github: ${options.github||'----'}`);
     console.log(`out: ${options.out||'report.md'}`);
+    console.log(`legacyScope: ${options.legacyscope||'----'}`);
+    console.log(`sarif: ${options.sarif||'----'}`);
+
+    let sarif;
+    if(options.sarif === undefined) sarif = undefined;
+    else if(options.sarif === true) sarif = 'analyzer.sarif';
+    else sarif = options.sarif;
+
     console.log('*****************************')
     // ============================== GENERATE REPORT ==============================
-    main(basePath, options.scope, options.gihub, options.out || 'report.md');
+    main(basePath, options.scope, options.github, options.out || 'report.md', options.legacyscope, sarif, options.listfiles);
   }).parse();
